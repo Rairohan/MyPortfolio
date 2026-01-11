7c92e092ee20f8672ccca84308fa6bdc0af5eb04
rohan <rairohan14303@gmail.com>
2026-01-11 10:41:53 +0545

    Feature: Added my tech stack section

---
+++ components/Tech.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+export default function Tech(){
+    return(
+        <div className="tech">
+            <h2>Skills & Technologies</h2>
+            <br />
+            <p>Here are the technologies and tools I use to bring ideas to life</p>
+            <main>
+                <ul className="Tech-list">
+                    <li> <img src="/icons8-html-5-100.png" alt="HTML5" /></li>
+                    <li><img src="/icons8-css3-100.png" alt="CSS3" /></li>
+                    <li><img src="/jslogo.png" alt="JavaScript" /></li>
+                    <li><img src="/React-Logo.png" alt="React" /></li>
+                    <li><img src="/gitogo.png" alt="GitHub" /></li>
+                    <li><img src="/mysql.png" alt="MySQL" /></li>
+                </ul>
+            </main>
+        </div>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import './index.css'
 import Body from '/components/Body.jsx'
 import Navbar from '/components/Navbar.jsx'
 import About from '/components/About.jsx'
+import Tech from '/components/Tech.jsx'
 createRoot(document.getElementById('root')).render(
  <>
   <Navbar />
   <Body/>
   <About />
+  <Tech />
  </>
 )
